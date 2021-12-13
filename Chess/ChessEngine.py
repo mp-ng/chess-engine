54d50db09a0f8b0652ffa9170959764beb29e83f
Ming Pok Ng <ngmingpok@gmail.com>
2021-12-13 23:30:13 +0000

    Complete get_p_moves

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -16,6 +16,8 @@ class GameState:
             ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
             ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
         ]
+        self.move_mappings = {'P': self.get_p_moves, 'R': self.get_r_moves, 'N': self.get_n_moves,
+                              'B': self.get_b_moves, 'K': self.get_k_moves, 'Q': self.get_q_moves}
         self.white_to_move = True
         self.move_log = []
 
@@ -41,24 +43,34 @@ class GameState:
         for r in range(len(self.board)):
             for c in range(len(self.board[r])):
                 color = self.board[r][c][0]
-                if color == 'w' and self.white_to_move or color == 'b' and not self.white_to_move:
+                if (color == 'w' and self.white_to_move) or (color == 'b' and not self.white_to_move):
                     piece = self.board[r][c][1]
-                    if piece == 'P':
-                        self.get_p_moves(r, c, moves)
-                    elif piece == 'R':
-                        self.get_r_moves(r, c, moves)
-                    elif piece == 'N':
-                        self.get_n_moves(r, c, moves)
-                    elif piece == 'B':
-                        self.get_b_moves(r, c, moves)
-                    elif piece == 'K':
-                        self.get_k_moves(r, c, moves)
-                    else:
-                        self.get_q_moves(r, c, moves)
+                    return self.move_mappings[piece]
         return moves
 
     def get_p_moves(self, r, c, moves):
-        pass
+        if self.white_to_move:
+            if self.board[r - 1][c] == '--':
+                moves.append(Move((r, c), (r - 1, c), self.board))
+                if r == 6 and self.board[r - 2][c] == '--':
+                    moves.append(Move((r, c), (r - 2, c), self.board))
+            if c > 0:
+                if self.board[r - 1][c - 1][0] == 'b':
+                    moves.append(Move((r, c), (r - 1, c - 1), self.board))
+            if c < 7:
+                if self.board[r - 1][c + 1][0] == 'b':
+                    moves.append(Move((r, c), (r - 1, c + 1), self.board))
+        else:
+            if self.board[r + 1][c] == '--':
+                moves.append(Move((r, c), (r + 1, c), self.board))
+                if r == 1 and self.board[r + 2][c] == '--':
+                    moves.append(Move((r, c), (r + 2, c), self.board))
+            if c > 0:
+                if self.board[r + 1][c - 1][0] == 'w':
+                    moves.append(Move((r, c), (r + 1, c - 1), self.board))
+            if c < 7:
+                if self.board[r + 1][c + 1][0] == 'w':
+                    moves.append(Move((r, c), (r + 1, c + 1), self.board))
 
     def get_r_moves(self, r, c, moves):
         pass
@@ -77,12 +89,12 @@ class GameState:
 
 
 class Move:
-    ranks_to_rows = {"1": 7, "2": 6, "3": 5, "4": 4,
-                     "5": 3, "6": 2, "7": 1, "8": 0}
+    ranks_to_rows = {'1': 7, '2': 6, '3': 5, '4': 4,
+                     '5': 3, '6': 2, '7': 1, '8': 0}
     rows_to_ranks = {v: k for k, v in ranks_to_rows.items()}
 
-    files_to_cols = {"a": 0, "b": 1, "c": 2, "d": 3,
-                     "e": 4, "f": 5, "g": 6, "h": 7}
+    files_to_cols = {'a': 0, 'b': 1, 'c': 2, 'd': 3,
+                     'e': 4, 'f': 5, 'g': 6, 'h': 7}
     cols_to_files = {v: k for k, v in files_to_cols.items()}
 
     def __init__(self, start_sq, end_sq, board):
@@ -92,6 +104,7 @@ class Move:
         self.piece_captured = board[self.end_row][self.end_col]
         # A unique ID in the range [0, 7777]
         self.move_ID = self.start_row * 1000 + self.start_col * 100 + self.end_row * 10 + self.end_col
+        print(self.move_ID)
 
     def __eq__(self, other):
         if isinstance(other, Move):
@@ -103,16 +116,16 @@ class Move:
         # Ambiguity is not be avoided for simplicity (can refer to stockfish code)
         name_moved = self.piece_moved[1]
         name_captured = self.piece_captured[1]
-        if name_moved == "P":
-            if name_captured == "-":
+        if name_moved == 'P':
+            if name_captured == '-':
                 return self.get_rank_file(self.end_row, self.end_col)
             else:
-                return self.cols_to_files[self.start_col] + "x" + self.get_rank_file(self.end_row, self.end_col)
+                return self.cols_to_files[self.start_col] + 'x' + self.get_rank_file(self.end_row, self.end_col)
         else:
-            if name_captured == "-":
+            if name_captured == '-':
                 return name_moved + self.get_rank_file(self.end_row, self.end_col)
             else:
-                return name_moved + "x" + self.get_rank_file(self.end_row, self.end_col)
+                return name_moved + 'x' + self.get_rank_file(self.end_row, self.end_col)
 
     def get_rank_file(self, r, c):
         return self.cols_to_files[c] + self.rows_to_ranks[r]
